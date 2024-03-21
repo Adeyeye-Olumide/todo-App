76dc39823ff8bbae3dd3e6b1a700e0049250f4f6
Elysium <eagleprince.ao@gmail.com>
2024-03-21 17:33:55 +0100

    coded the basic functionality of the todo app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import React, { ChangeEventHandler, useState } from 'react';
+import React, { ChangeEventHandler, useState, useEffect, useCallback } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import InputComponent from './inputComponent/inputComponent';
-import { FormEvent, ChangeEvent,} from 'react';
+import { FormEvent, ChangeEvent, MouseEvent} from 'react';
+import CardComponent from './card-component/CardComponent';
+import { IconType } from 'react-icons';
 
 export interface Todo {
   todo : string, 
@@ -10,10 +12,21 @@ export interface Todo {
   isDone: boolean
 }
 
+
+export interface Handler {
+  isDoneHandler: (this: HTMLDivElement, e: globalThis.MouseEvent) => any,
+  manageEditHandler: (this: HTMLDivElement, e: globalThis.MouseEvent) => any,
+  editHandler: ( e: FormEvent)=> void
+}
+
+
+
 function App() {
   const [todoArray, setTodoArray] = useState<Todo[]>([])
 
-  const addToArray = (e: ChangeEvent<HTMLFormElement>)=> {
+  let iconTargetTagName: string = ''
+
+  const addToArray = useCallback((e: ChangeEvent<HTMLFormElement>)=> {
 
     e.preventDefault()
     
@@ -24,8 +37,124 @@ function App() {
     inputEl.blur()
 
 
+  }, [todoArray])
+   
+
+  const isDoneHandler : Handler["isDoneHandler"] = ( e: globalThis.MouseEvent) => {
+   
+
+    const iconTarget = (e.target as SVGElement) || (e.target as SVGPathElement)
+
+    console.log(iconTarget)
+
+
+    
+
+    if (iconTarget.closest(".isDone__icon")) setTodoArray(todoArray.map((todos, i, array) => {
+
+      const iconTargetString = (iconTarget?.closest(".todo__single")as HTMLFormElement).querySelector(".todo__single--text")?.textContent
+
+      if(todos.todo == iconTargetString) return {...todos, isDone: !todos.isDone}
+      
+      return todos
+    
+    }))
+    
+
+    
+
+    if (iconTarget.closest(".delete__icon")) setTodoArray([...todoArray].filter(todos => todos.todo != (iconTarget?.closest(".todo__single")as HTMLFormElement).querySelector(".todo__single--text")?.textContent))
+
+
+    console.log(todoArray)
+    
   }
 
+  const editHandler: Handler["editHandler"] = useCallback((e: FormEvent)=> {
+    console.log("yes")
+
+    e.preventDefault()
+
+
+    const formTarget = (e.target as HTMLFormElement)
+    const inputEl = (formTarget.querySelector('.todo__single--edit') as HTMLInputElement)
+    let string = inputEl.defaultValue
+
+    console.log(string)
+
+   
+
+    console.log(formTarget)
+
+    setTodoArray(todoArray.map(todos => todos.todo == string?
+    {...todos, todo: inputEl.value}: todos
+    ))
+
+    console.log(todoArray)
+
+
+
+
+
+
+
+    
+  }, [todoArray])
+
+
+  const deleteTodo : Handler["isDoneHandler"] = (e: globalThis.MouseEvent)=> {
+
+    const iconTarget = (e.target as SVGElement)
+
+    if (!iconTarget.classList.contains("delete__icon")) return
+
+    console.log('deleting')
+
+
+    setTodoArray([...todoArray].filter(todos => todos.id.toString() != (iconTarget?.closest('.todo__single') as HTMLFormElement).id))
+
+
+  }
+
+  // const manageEditHandler:  Handler["manageEditHandler"] = (e: globalThis.MouseEvent) => {
+
+  //   // e.preventDefault()
+
+  //   if ((e.target as HTMLFormElement).classList.contains('todo__single')) return
+
+  //   const allForm = (document.querySelectorAll('.todo__single') as NodeList)
+
+  //   allForm.forEach(form => (form as HTMLFormElement).id == (e.target as HTMLFormElement).id? (form as HTMLFormElement).addEventListener("submit", editHandler)
+  //   : (form as HTMLFormElement).removeEventListener("submit", editHandler))
+
+
+  // }
+
+
+
+
+  useEffect(()=> {
+
+   
+    const element = (document.querySelector('.App') as HTMLDivElement)
+    
+
+    element.addEventListener("click", isDoneHandler)
+    
+    
+   
+
+    return ()=> {
+      element.removeEventListener("click", isDoneHandler)
+      
+      
+      
+    }
+
+
+
+
+  }, [todoArray])
 
   
 
@@ -36,8 +165,9 @@ function App() {
       <span className='heading'>Taskify</span>
       <InputComponent addToArray={addToArray}/>
       
-        {todoArray.map(({todo, id, isDone})=> 
-          <li key={id}>{todo}</li>
+        {todoArray.map((todos, index)=> 
+
+          <CardComponent todos = {todos}  key = {index} id = {index.toString()} editHandler = {editHandler}></CardComponent>
         )}
     
       
--- src/card-component/CardComponent.tsx
+++ src/card-component/CardComponent.tsx
@@ -1,12 +1,77 @@
-import React from 'react'
+import React, { useState, MouseEventHandler, useEffect } from 'react'
 
-import { Todo } from '../App'
+import { Todo, Handler} from '../App'
 
-function CardComponent({todo} : Todo) {
+
+import {AiFillEdit, AiFillDelete} from 'react-icons/ai'
+import {MdDone} from 'react-icons/md'
+
+import './file.css'
+
+interface props {
+    todos: Todo,
+    key: number,
+    id : string,
+    editHandler : Handler['editHandler']
+    
+    
+}
+
+
+
+
+
+function CardComponent({todos, id, editHandler} : props) {
+
+    const [edit, setEdit] = useState<boolean>(false)
+
+    useEffect(()=> {
+
+        setEdit(false)
+    }, [todos.todo])
+
+
+
+
   return (
-    <div>
+    <form className='todo__single' id ={id} onSubmit = {
+        
+        (e)=> {
+            editHandler(e)
+        }}>
+       
+        {   
+             edit? <input type="text" className= 'todo__single--edit' defaultValue={todos.todo}/>
+            :
+            (
+            todos.isDone?
+            <s className='todo__single--text'>{todos.todo}</s> :
+            <span className='todo__single--text'>{todos.todo}</span>
+            )
+
+           
+            
+        }
+       
+        <div>
+            <span className='icon edit__icon'>
+                <AiFillEdit onClick={()=> setEdit(!edit)}></AiFillEdit>
+
+            </span>
+            <span className='icon delete__icon'>
+                <AiFillDelete></AiFillDelete>
+        
+            </span>
+            <span className='icon'>
+                <MdDone className='isDone__icon'></MdDone>
+        
+            </span>
+
+        </div>
+        
+
       
-    </div>
+    </form>
   )
 }
 
